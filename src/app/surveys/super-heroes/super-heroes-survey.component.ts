80bf4481055276424143e3a46b07a8a103d778b8
Chandler-Gegg <chandlergegg@gmail.com>
2017-05-22 07:58:30 -0500

    working on classes for selecting answers

--- src/app/surveys/super-heroes/super-heroes-survey.component.ts
+++ src/app/surveys/super-heroes/super-heroes-survey.component.ts
@@ -1,19 +1,36 @@
 import { Component }              from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-// import {SurveyQuestion} from '../../shared/classes/question';
 import {QuizDataService} from '../../shared/services/quiz-data.service';
 
 @Component({
   selector: 'super-heroes-survey',
-  templateUrl: './super-heroes-survey.component.html'
+  templateUrl: './super-heroes-survey.component.html',
+  styleUrls: ['./super-heroes-survey.component.scss']
 })
 
 export class SuperHeroesSurveyComponent {
-    // surveyQuestions: SurveyQuestion;
-    quiz;    
-    constructor(private quizDataService : QuizDataService) {
-        // this.surveyQuestions = this.quizDataService.getQuizData();
-        this.quiz = this.quizDataService.getQuizData();
+    quiz;
+    surveyResponse:any = {};
+    submitted = false;
+    constructor(private quizDataService : QuizDataService) {        
+        this.quiz = this.quizDataService.getQuizData();                
+    }
+    selectAnswer(surveyQuestion, answer){
+        this.surveyResponse[surveyQuestion.question] = {};
+        this.surveyResponse[surveyQuestion.question].userAnswer = answer;
+        this.surveyResponse[surveyQuestion.question].correctAnswer = surveyQuestion.answers[surveyQuestion.correct-1];
+        console.log('SELECT ANSWER');
+        console.log(this.surveyResponse[surveyQuestion.question]);
+        console.log('SELECT ANSWER');
+    }
+
+    onSubmit(event) {
+        this.submitted = true;
+        console.log('SUMBIT SURVEY ....');
+        console.log(this.surveyResponse);     
+        console.log('SUMBIT SURVEY ....');
+        // this.router.navigate(['/survey-results']);
     }
 
 }
+
